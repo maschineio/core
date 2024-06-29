70ae3e0f7812159f8b31f12749026d29bf720187
Thomas Meitz <info@jproxx.com>
2024-06-29 21:28:24 +0200

    make sonar happy

--- zap-mask.go
+++ zap-mask.go
@@ -7,7 +7,6 @@ import (
 )
 
 // MaskSensitiveData
-// TODO: its not only password, it shoult be a regex, and also users are also sensitive
 func MaskSensitiveData(e zapcore.Entry) error {
 	e.Message = strings.Replace(e.Message, "dbpass", "**masked**", -1)
 	return nil
