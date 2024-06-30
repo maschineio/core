bcc58df3f58e5e675bdcb8adef462210ea672591
Thomas Meitz <info@jproxx.com>
2024-06-30 10:22:04 +0200

    test context credentials exists

--- context/context_test.go
+++ context/context_test.go
@@ -25,3 +25,22 @@ func TestContextGetCredentialsNonExist(t *testing.T) {
 	assert.Nil(t, result)
 	assert.False(t, exists)
 }
+
+func TestContextGetCredentialsExist(t *testing.T) {
+	ctx := context.Context{}
+	ctx.Set(context.CREDENTIALSKEY, map[string]any{"user": "test"})
+	result, exists := ctx.GetCredential("user")
+	credExists := ctx.CredentialsExists()
+
+	assert.NotNil(t, result)
+	assert.True(t, exists)
+	assert.True(t, credExists)
+	assert.Equal(t, "test", result)
+}
+
+func TestContextGetCredentialsExistsFalse(t *testing.T) {
+	ctx := context.Context{}
+	result := ctx.CredentialsExists()
+
+	assert.False(t, result)
+}
