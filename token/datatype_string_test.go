b1c50d7f9d538af1bb617f3cde9769109112b4af
Thomas Meitz <info@jproxx.com>
2024-06-30 13:07:46 +0200

    added test for DataType(-1)

--- token/datatype_string_test.go
+++ token/datatype_string_test.go
@@ -9,6 +9,13 @@ import (
 
 func TestDataTypeString(t *testing.T) {
 	tok := token.String
-
 	assert.Equal(t, "String", tok.String())
+}
+
+func TestDataTypeError(t *testing.T) {
+	testCases := []token.DataType{
+		-1, // less than 0
+	}
+	result := testCases[0].String()
+	assert.Equal(t, "DataType(-1)", result)
 }
