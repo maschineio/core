692ca7cb12aea2bb4387e6bef0cb672f772b19f9
Thomas Meitz <info@jproxx.com>
2024-06-30 16:56:08 +0200

    more value type tests

--- value-type_test.go
+++ value-type_test.go
@@ -19,3 +19,39 @@ func TestValueTypeStringMapString(t *testing.T) {
 //	result := testCases[0].String()
 //	assert.Equal(t, "DataType(-1)", result)
 //}
+
+func TestGetValueTypeByteSlice(t *testing.T) {
+	v, err := core.GetTypedValue([]byte("hello"))
+	assert.Nil(t, err)
+	assert.Equal(t, "Bytes", v.Type().String())
+}
+
+func TestGetValueTypeBool(t *testing.T) {
+	v, err := core.GetTypedValue(false)
+	assert.Nil(t, err)
+	assert.Equal(t, "Bool", v.Type().String())
+}
+
+func TestGetValueTypeStringMap(t *testing.T) {
+	v, err := core.GetTypedValue(map[string]any{"key": "value"})
+	assert.Nil(t, err)
+	assert.Equal(t, "StringMap", v.Type().String())
+}
+
+func TestGetValueTypePtrStringMap(t *testing.T) {
+	v, err := core.GetTypedValue(&map[string]any{"key": "value"})
+	assert.Nil(t, err)
+	assert.Equal(t, "PointerStringMap", v.Type().String())
+}
+
+func TestGetValueTypeSliceVal(t *testing.T) {
+	v, err := core.GetTypedValue([]any{1, 2, 3})
+	assert.Nil(t, err)
+	assert.Equal(t, "Slice", v.Type().String())
+}
+
+func TestGetValueTypeInt(t *testing.T) {
+	v, err := core.GetTypedValue(int(42))
+	assert.Nil(t, err)
+	assert.Equal(t, "Float", v.Type().String()) // we are dealing with JSON float all the time
+}
