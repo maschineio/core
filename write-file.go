728b8b0be60492cf4d5c123f6ab764668a9b7424
Thomas Meitz <info@jproxx.com>
2024-12-25 12:50:59 +0100

    fix: gosec WriteFile

--- write-file.go
+++ write-file.go
@@ -6,7 +6,7 @@ import (
 
 func WriteFile(fileName string, data []byte) error {
 	// Write the byte stream to the file using os.WriteFile
-	err := os.WriteFile(fileName, data, 0640)
+	err := os.WriteFile(fileName, data, 0600)
 	if err != nil {
 		return err
 	}
