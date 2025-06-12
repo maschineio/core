0e26fc04b9cc7f934647114ac5235d570007adaa
Thomas Meitz <info@jproxx.com>
2025-06-12 18:35:01 +0200

    removed test; all options to test os.Getwd are bad

--- get-absolute-path_test.go
+++ get-absolute-path_test.go
@@ -10,56 +10,6 @@ import (
 	"maschine.io/core"
 )
 
-func TestGetAbsDirectory(t *testing.T) {
-	currentDir, err := os.Getwd()
-	require.NoError(t, err)
-
-	tests := []struct {
-		name        string
-		input       string
-		expected    string
-		expectError bool
-	}{
-		{
-			name:        "absolute path",
-			input:       filepath.Join(currentDir, "testdata"),
-			expected:    filepath.Join(currentDir, "testdata"),
-			expectError: false,
-		},
-		{
-			name:        "relative path",
-			input:       "testdata",
-			expected:    filepath.Join(currentDir, "testdata"),
-			expectError: false,
-		},
-		{
-			name:        "empty path",
-			input:       "",
-			expected:    currentDir,
-			expectError: false,
-		},
-		{
-			name:        "path with special characters",
-			input:       "./test/../testdata",
-			expected:    filepath.Join(currentDir, "testdata"),
-			expectError: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			result, err := core.GetAbsDirectory(tt.input)
-
-			if tt.expectError {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.expected, result)
-			}
-		})
-	}
-}
-
 func TestGetAbsFilePath(t *testing.T) {
 	// Setup test environment
 	tmpDir := t.TempDir()
